3feb6a67e1a6c1a8c4c1a3892355b7787da77c95
marson parulian <marson.parulian@gmail.com>
2021-08-04 22:35:11 +1000

    delete unused files

--- test/first.test.ts
+++
@@ -1,8 +0,0 @@
-import first from "../src/first";
-
-describe("Firt test suitcase", () => {
-    test("This is the first case", () => {
-        console.log(first());
-        expect(true).toBe(true);
-    });
-})
