e93042eb4c5b9034129674fc198613fd2dfca0b5
marson parulian <marson.parulian@gmail.com>
2021-08-07 23:49:47 +1000

    init interface for generic DAO

--- src/services/db/db.ts
+++ src/services/db/db.ts
@@ -1,6 +1,12 @@
 // This file implements `db` implementation
 import dbMongoDb from "./mongodb/mongodb";
 
+// Interface for generic DAO
+export interface DAO {
+    save: () => Promise<void>,
+}
+
+// Interface of DB instance
 export interface DbInterface {
     connect: () => Promise<void>,
     disconnect: () => Promise<void>,
