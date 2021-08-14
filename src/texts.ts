87286862e5803ed29b657ef1aa0aff293e6b9fcd
marson parulian <marson.parulian@gmail.com>
2021-08-15 06:57:59 +1000

    add texts / messages related to suburb invalid save

--- src/texts.ts
+++ src/texts.ts
@@ -1,6 +1,10 @@
 // This file contains textx / messages, MOSTLY FOR RESPONSES.
 const texts = {
     SAVED: "Saved",
+    // Suburb related
+    SUBURB_NAME_REQUIREDl: "Suburb name is required",
+    SUBURB_POSTCODE_REQUIRED: "Suburb post code is required",
+    SUBURB_POSTCODE_INVALID: "Invalid format. Post code should contain 4 numbers",
 }
 
 export default texts;
