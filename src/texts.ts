5fa1766d857dcdf2808e17c03f0d7cf0fc1e6377
marson parulian <marson.parulian@gmail.com>
2021-08-17 07:08:37 +1000

    fix typo

--- src/texts.ts
+++ src/texts.ts
@@ -2,7 +2,7 @@
 const texts = {
     SAVED: "Saved",
     // Suburb related
-    SUBURB_NAME_REQUIREDl: "Suburb name is required",
+    SUBURB_NAME_REQUIRED: "Suburb name is required",
     SUBURB_NAME_INVALID: "Suburb name is invalid. Should only contain alphanumeric",
     SUBURB_POSTCODE_REQUIRED: "Suburb post code is required",
     SUBURB_POSTCODE_INVALID: "Invalid format. Post code should contain 4 numbers",
