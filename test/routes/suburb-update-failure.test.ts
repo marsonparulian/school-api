294f78fa451b3ac33add9d442ff8fe70990c221c
marson parulian <marson.parulian@gmail.com>
2021-08-18 10:15:08 +1000

    update suburb - init specs

---
+++ test/routes/suburb-update-failure.test.ts
@@ -0,0 +1,10 @@
+import supertest from "supertest";
+import app from "../../src/app";
+
+// Update suburb test
+describe("Invalid update suburb route test", () => {
+    test.todo("Invalid id");
+    test.todo("id not exist in DB");
+    test.todo("name is falsy");
+    test.todo("postCode is falsy");
+})
