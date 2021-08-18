83b0bafa04bae2d3448ed1995e208d2485310ab4
marson parulian <marson.parulian@gmail.com>
2021-08-18 19:58:36 +1000

    middleware - create middleware to check _id in request.params

--- src/middlewares/validator.middleware.ts
+++ src/middlewares/validator.middleware.ts
@@ -1,5 +1,6 @@
 // This file contains middlewares related to request validation
 import { Request, Response, NextFunction } from "express";
+import db from "../services/db/db";
 import texts from "../texts";
 
 /**
@@ -7,10 +8,18 @@ import texts from "../texts";
  * If not, response with `400 Bad Request`
  */
 export const idShouldExistAndValidInParams = (req: Request, res: Response, next: NextFunction): void => {
-    // Check and next()
-    if (req.params._id) next();
+    // Is `_id` valid ?
+    const _id = req.params._id;
+    const isValid = _id && db.isIdValid(_id);
 
-    // Catched 
+    // Is id valid ?
+    if (isValid) {
+        // Continue to next middleware
+        next();
+        return;
+    }
+
+    // Not valid, send response
     res.status(400).send({
         message: texts.VALID_ID_REQUIRED,
     })
