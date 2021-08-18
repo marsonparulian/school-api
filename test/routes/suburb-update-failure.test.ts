2b7d48d37dc40b2dfda330a7857e9970b0567fef
marson parulian <marson.parulian@gmail.com>
2021-08-19 07:09:38 +1000

    update suburb - add test for falsy 'name'

--- test/routes/suburb-update-failure.test.ts
+++ test/routes/suburb-update-failure.test.ts
@@ -6,6 +6,10 @@ import "dotenv/config";
 
 // Update suburb test
 describe("Invalid update suburb route test", () => {
+    beforeAll(async () => {
+        // Connect to DB
+        await db.connect();
+    })
     test("id is not provided", async () => {
         // Make request. `id` not provided in URI 
         const response = await supertest(app)
@@ -39,9 +43,6 @@ describe("Invalid update suburb route test", () => {
         })
     });
     test("id not exist in DB", async () => {
-        // Connect to DB
-        await db.connect();
-
         // Make request with non existing `_id`
         const response = await supertest(app)
             .put("/api/suburb/012345678901234567890123")
@@ -56,9 +57,43 @@ describe("Invalid update suburb route test", () => {
             message: texts.ID_NOT_EXIST,
         })
 
+    });
+    test("name is falsy", async () => {
+        // Save new suburb
+        const saveResponse = await supertest(app)
+            .post("/api/suburb")
+            .send({ name: "Lidcombe", postCode: "4523" })
+            .catch((e) => {
+                throw (e);
+            });
+
+        // Response status should be 201
+        expect(saveResponse.status).toBe(200);
+
+        // Get the saved id
+        const _id = saveResponse.body.suburb._id;
+
+        // Make request with falsy `name
+        const response = await supertest(app)
+            .put(`/api/suburb/${_id}`)
+            .send({ postCode: "3456" })
+            .catch((e) => {
+                throw (e);
+            });
+
+        // Response status should be 422
+        expect(response.status).toBe(422);
+        // Response body should contain 'name is required' msg
+        expect(response.body).toEqual(expect.objectContaining({
+            message: expect.any(String),
+            errors: expect.objectContaining({
+                name: texts.SUBURB_NAME_REQUIRED,
+            })
+        }))
+    });
+    test.todo("postCode is falsy");
+    afterAll(async () => {
         // Disconnect DB
         await db.disconnect();
-    });
-    test.todo("name is falsy");
-    test.todo("postCode is falsy");
+    })
 })
