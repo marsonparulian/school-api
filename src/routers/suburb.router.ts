3defa283448b4f9b3d4638e8570cdd6aa060d381
marson parulian <marson.parulian@gmail.com>
2021-08-18 15:47:37 +1000

    update suburb - handle unavailable _id

--- src/routers/suburb.router.ts
+++ src/routers/suburb.router.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import suburbSaveController from "../controllers/suburb/suburbSave.controller";
+import { idShouldExistAndValidInParams } from "../middlewares/validator.middleware";
 
 // Router for `suburb` request
 const router = Router();
@@ -7,6 +8,8 @@ const router = Router();
 // Handle request to save
 router.post("/", suburbSaveController);
 
+// Handle request to update suburb
+router.put("/:_id?", idShouldExistAndValidInParams, suburbSaveController);
 
 // Export router
 export default router;
--- test/routes/suburb-update-failure.test.ts
+++ test/routes/suburb-update-failure.test.ts
@@ -3,6 +3,19 @@ import app from "../../src/app";
 
 // Update suburb test
 describe("Invalid update suburb route test", () => {
+    test("id is not provided", async () => {
+        // Make request. `id` not provided in URI 
+        const response = await supertest(app)
+            .put("/api/suburb/")
+            .send({})
+            .catch((e) => {
+                throw (e)
+            });
+
+        // Response should be 401 Bad Request
+        expect(response.status).toBe(400);
+        // Response should contain 'id is required'
+    });
     test.todo("Invalid id");
     test.todo("id not exist in DB");
     test.todo("name is falsy");
