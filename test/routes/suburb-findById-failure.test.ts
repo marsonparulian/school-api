ee9389c6a3a50b4085a11ec9ae325f27bc6ebc9b
marson parulian <marson.parulian@gmail.com>
2021-08-21 08:05:30 +1000

    get suburb by id - remove not applicable spec

--- test/routes/suburb-findById-failure.test.ts
+++ test/routes/suburb-findById-failure.test.ts
@@ -5,7 +5,6 @@ import texts from "../../src/texts";
 
 // This file contain test cases for unsuccessful GET 'suburb by id' request.
 describe("Get suburb by id - failures", () => {
-    test.todo("id is not provided");
     test.todo("id is not valid");
     test.todo("id not exist in DB");
 });
