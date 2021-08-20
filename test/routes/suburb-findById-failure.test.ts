e5a7d1a9d90e3dac04184585acc51ff2eff1a5c4
marson parulian <marson.parulian@gmail.com>
2021-08-21 07:52:20 +1000

    get suburb by id - init specs

---
+++ test/routes/suburb-findById-failure.test.ts
@@ -0,0 +1,11 @@
+import supertest from "supertest";
+import app from "../../src/app";
+import db from "../../src/services/db/db";
+import texts from "../../src/texts";
+
+// This file contain test cases for unsuccessful GET 'suburb by id' request.
+describe("Get suburb by id - failures", () => {
+    test.todo("id is not provided");
+    test.todo("id is not valid");
+    test.todo("id not exist in DB");
+});
