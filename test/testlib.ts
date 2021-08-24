372d31ac4f2a36c71029f7dc037d3ce310b5413e
marson parulian <marson.parulian@gmail.com>
2021-08-25 08:16:24 +1000

    init test library

---
+++ test/testlib.ts
@@ -0,0 +1,10 @@
+// This file contain data that can be used repeatedly in tests
+const lib = {
+    //'Data's below not contain id or reference property since the `_id`s are generated in DB
+    suburb1Data: { name: "Sydney", postCode: "2000" },
+    suburb2Data: { name: "Paramatta", postCode: "2150" },
+    suburb3Data: { name: "Blacktown", postCode: "2148" },
+    school1Data: { name: "Sydney Boys" },
+    school2Data: { name: "Paramatta Public" }
+}
+export default lib;
