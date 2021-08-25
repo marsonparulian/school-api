c590a9c6290d1863d41b8bd87ba4edb3dc980b32
marson parulian <marson.parulian@gmail.com>
2021-08-26 07:11:05 +1000

    static - add Ui messages

--- src/texts.ts
+++ src/texts.ts
@@ -1,10 +1,13 @@
 // This file contains textx / messages, MOSTLY FOR RESPONSES.
 const texts = {
     SAVED: "Saved",
+    save_failure: "Save failed.",
     FETCH_SUCCESS: "Fetch success",
     FETCH_FAILURE: "Fetch failed.",
     DELETE_SUCCESS: "Delete success.",
+    DELETE_FAILURE: "Delete failed.",
     // Validation related
+    REQUIRED: "Required.",
     VALID_ID_REQUIRED: "Valid id is required",
     ID_NOT_EXIST: "Id not exist",
     // Suburb related
