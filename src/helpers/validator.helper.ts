a7cb7c01ab4d3a0af0a13d9254196e3ab1aed0d5
marson parulian <marson.parulian@gmail.com>
2021-08-26 21:52:20 +1000

    create custom validator to check if a docuemnt with specific _id exist in dB

--- src/helpers/validator.helper.ts
+++ src/helpers/validator.helper.ts
@@ -1,6 +1,7 @@
 // Helpers for validation related operations
 
-import { ErrorMessages } from "../types/common";
+import { Entity, ErrorMessages } from "../types/common";
+import { DAO } from "../services/db/db";
 
 /**
  * Create key-text pairs for error messages based on `express-validator.validationResult..array()`.
@@ -17,4 +18,25 @@ export const createErrorMessages = (
         acc[param] = msg;
         return acc;
     }, {});
+}
+
+/**
+ *  Custom validator to check if an implementation of DAO<T> exist in DB
+ * Return promise. Will resolve if the `T`    exist and will reject if not found.
+ * @param dao {DAO<T>} - DAO<T> implementation.
+ * @param _id {String} - id of Entity} to look for.
+ */
+export const isEntityExistInDB = async (dao: DAO<any>, _id: string): Promise<void> => {
+    try {
+        const result = await dao.findById(_id);
+
+        // Resolve if truthy (found)
+        if (result) return Promise.resolve();
+        // Reject
+        return Promise.reject();
+
+    } catch (e) {
+        console.error(`Fatal error : Unable to complete DB operation to find by id : ${_id}`)
+        console.error(e);
+    }
 }
