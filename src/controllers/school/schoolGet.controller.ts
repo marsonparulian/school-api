befb5d8b7c6cf9022385c764b04cec0fc9f8d39c
marson parulian <marson.parulian@gmail.com>
2021-08-28 14:18:06 +1000

    fetch school - andle request for list of school

--- src/controllers/school/schoolGet.controller.ts
+++ src/controllers/school/schoolGet.controller.ts
@@ -6,9 +6,23 @@ import texts from "../../texts";
  * Handle request for schools
  */
 export const getSchool = async (req: Request, res: Response): Promise<void> => {
-    res.status(501).json({
-        message: "Not implemented"
-    });
+    try {
+        // DB call
+        const schools = await db.school.find().catch((e) => {
+            throw (e);
+        });
+
+        // Response
+        res.status(200).json({
+            message: texts.FETCH_SUCCESS,
+            schools
+        })
+    } catch (e) {
+        res.status(500).json({
+            message: texts.FETCH_FAILURE,
+            error: e.message,
+        });
+    }
 }
 /**
  * Handle request for school with specific id
--- test/routes/school-get.test.ts
+++ test/routes/school-get.test.ts
@@ -34,8 +34,61 @@ describe("GET /api/school", () => {
         // Response status should be 200
         expect(response.status).toBe(200);
     });
-    test.todo("GET 1 schools");
-    test.todo("GET 3 schools");
+    test("GET 1 schools", async () => {
+        // Prepare data
+        const schoolData = testlib.school4Data;
+
+        // Save new school (and related suburb)
+        await testlib.createSchoolAndSuburb(schoolData, testlib.suburb4Data);
+
+        // Make request to fetch suburb
+        const response = await supertest(app)
+            .get("/api/school").catch((e) => {
+                throw (e);
+            });
+
+        // Request body should contain `suburbs` property with  array value
+        expect(response.body).toEqual(expect.objectContaining({
+            message: texts.FETCH_SUCCESS,
+            schools: expect.any(Array),
+        }));
+        // `request.body.schools` should 1 element array
+        expect(response.body.schools.length).toBe(1);
+        // Response body should contain the saved school
+        expect(response.body.schools[0]).toEqual(expect.objectContaining(schoolData,));
+        // Response status should be 200
+        expect(response.status).toBe(200);
+    });
+    test("GET 3 schools", async () => {
+        // Prepare data
+        const school1Data = testlib.school5Data;
+        const school2Data = testlib.school6Data;
+
+        // Create a new school & suburb
+        await testlib.createSchoolAndSuburb(school1Data, testlib.suburb4Data);
+
+        // Create another news school & suburb
+        await testlib.createSchoolAndSuburb(school2Data, testlib.suburb4Data);
+
+        // Make request to fetch schools
+        const response = await supertest(app)
+            .get("/api/school/").catch((e) => {
+                throw (e);
+            })
+
+        // Response body should have `schools` property with Array value
+        expect(response.body).toEqual(expect.objectContaining({
+            message: texts.FETCH_SUCCESS,
+            schools: expect.any(Array),
+        }));
+        //`response.body.schools` should have 3 elements
+        expect(response.body.schools.length).toBe(3);
+        // The `response.body.schools` should contain the 2 newly created schools
+        expect(response.body.schools).toEqual(expect.arrayContaining([expect.objectContaining(school1Data)]));
+        expect(response.body.schools).toEqual(expect.arrayContaining([expect.objectContaining(school2Data)]));
+        // Response status should be 200
+        expect(response.status).toBe(200);
+    });
     test.todo("GET by school id ");
     afterAll(async () => {
         // Disconnect
