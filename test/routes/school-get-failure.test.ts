0a9b91f53bdf770958a3616c09236c0a01079891
marson parulian <marson.parulian@gmail.com>
2021-08-28 16:09:18 +1000

    fetch school - init specs for failed cases

---
+++ test/routes/school-get-failure.test.ts
@@ -0,0 +1,19 @@
+import supertest from "supertest";
+import app from "../../src/app";
+import db from "../../src/services/db/db";
+import texts from "../../src/texts";
+// This file contain test cases for invalid GET `/api/school/`
+
+describe("GET /api/school - failed cases", () => {
+    beforeAll(async () => {
+        // Connect to DB
+        await db.connect();
+    });
+    test.todo("GET with missing '_id'");
+    test.todo("GET with falsy 'id'");
+    test.todo("GET with non existing 'id'");
+    afterAll(async () => {
+        // DIsconnect
+        await db.disconnect();
+    })
+});
