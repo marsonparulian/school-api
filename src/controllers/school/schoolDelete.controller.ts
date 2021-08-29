fd7014960d629f04e6765b3aa88e8a783c111146
marson parulian <marson.parulian@gmail.com>
2021-08-29 21:41:16 +1000

    delete school - handle delete school by id

--- src/controllers/school/schoolDelete.controller.ts
+++ src/controllers/school/schoolDelete.controller.ts
@@ -1,12 +1,32 @@
 import { Request, Response, NextFunction } from "express";
 import db from "../../services/db/db";
 import { idShouldExistAndValidInParams, middlewareIdInParamsShouldExistInDb } from "../../middlewares/validator.middleware";
+import schoolModel from "../../services/db/mongodb/models/schoolModel";
+import texts from "../../texts";
 
 /**
  * Middleware to delete school by id
  */
 export const deleteSchoolById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
-    res.status(501).json({ message: "Not implemented" });
+    try {
+        // Delete school
+        const school = await db.school.findByIdAndDelete(req.params._id);
+
+        // Response
+        res.status(200).json({
+            // if `null`, nothing has been deleted.
+            message: school ? texts.DELETE_SUCCESS : texts.DELETE_FAILURE,
+            school,
+        })
+    } catch (e) {
+        // Unknown error. Send response
+        res.status(500).json({
+            message: "Unknow error deleting school",
+            school: {
+                _id: req.params._id
+            }
+        })
+    }
 }
 
 const handlers = [
--- test/routes/school-delete.test.ts
+++ test/routes/school-delete.test.ts
@@ -56,7 +56,47 @@ describe("DELETE /api/school/_id", () => {
         // Request status should be 404
         expect(response.status).toBe(404);
     });
-    test.todo("Successful deleetion");
+    test("Successful deleetion", async () => {
+        // Prepare data
+        const school1Data = testlib.school6Data;
+
+        // Create school & suburb
+        const [school1Id, suburb1Id] = await testlib.createSchoolAndSuburb(school1Data, testlib.suburb5Data);
+
+        // DB call to verify the school existed in DB
+        const fetched1School = await db.school.findById(school1Id).catch((e) => {
+            throw (e);
+        });
+
+        // The result should have school1Data
+        expect(fetched1School).toEqual(expect.objectContaining(school1Data));
+
+        // Make request to delete school by id
+        const deleteResponse = await supertest(app)
+            .delete(`/api/school/${school1Id}`)
+            .catch((e) => {
+                throw (e);
+            });
+
+        // Response body should contain the data of the deleted school
+        expect(deleteResponse.body).toEqual(expect.objectContaining({
+            message: texts.DELETE_SUCCESS,
+            school: expect.objectContaining({
+                _id: school1Id,
+                ...school1Data,
+            }),
+        }));
+        // Response status should be 200
+        expect(deleteResponse.status).toBe(200);
+
+        // Make DB call to verify the school no longer exist in DB
+        const fetched2School = await db.school.findById(school1Id).catch((e) => {
+            throw (e);
+        });
+
+        // The result should be `null`
+        expect(fetched2School).toBeNull();
+    });
     afterAll(async () => {
         // Disconnect
         await db.disconnect();
