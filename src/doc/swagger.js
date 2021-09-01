9f6961d26fc2bfa34576de6242d663c1a21ed8c0
marson parulian <marson.parulian@gmail.com>
2021-09-01 22:46:22 +1000

    doc - generate swagger file directly to 'dist' folder'

--- src/doc/swagger.js
+++ src/doc/swagger.js
@@ -1,7 +1,7 @@
 const swaggerAutogen = require("swagger-autogen");
 
 // Paths are relative to root dir
-const outputFile = "src/doc/swagger_output.json";
+const outputFile = "dist/doc/swagger_output.json";
 const endpointFile = ["src/app.ts"];
 
 // Swagger document options
