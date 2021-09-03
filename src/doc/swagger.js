4089639691b226c75afd61e5a7c4bd73ab9fea66
marson parulian <marson.parulian@gmail.com>
2021-09-03 11:45:34 +1000

    doc - update vars

--- src/doc/swagger.js
+++ src/doc/swagger.js
@@ -1,17 +1,30 @@
+const { exit } = require("process");
 const swaggerAutogen = require("swagger-autogen");
+require("dotenv").config();
 
 // Paths are relative to root dir
 const outputFile = "src/doc/swagger_output.json";
 const endpointFile = ["src/app.ts"];
+
+// Check prerequisites
+if (!process.env.A_HOST) {
+    console.error("Missing env variables : 'A_HOST'");
+    exit();
+}
+if (!process.env.A_PORT) {
+    console.error("Missing env variable : A_PORT");
+    exit();
+}
 
 // Swagger document options
 const doc = {
     info: {
         version: "1.0.0",
         title: "Simple School API",
-        description: "Simple demo API to do CRUd operations to school / suburb.",
+        description: "This is a simple demoAPI site to Create-Retrieve-Update-Delete (CRUD) schools and related suburbs.",
     },
-    host: "localhost:8080",
+    // host:"localhost:8080",
+    host: `${process.env.A_HOST}:${process.env.A_PORT}`,
     consumes: ["application/json"],
     tags: [
         { name: "Suburb", description: "Suburb CRUD" },
