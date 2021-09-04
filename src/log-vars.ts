815c178eaa13e6fee719cefda98f8ca976ca089a
marson parulian <marson.parulian@gmail.com>
2021-09-04 22:10:27 +1000

    gh - set env vars from 'secrets'

--- src/log-vars.ts
+++ src/log-vars.ts
@@ -1,5 +1,5 @@
 // This file is for temporary use only. TO test environment vars in github action hosted runner
 import "dotenv/config";
 
-console.log("mpa environment vars : ", process.env);
 console.log("mpa mongodb pass : ", process.env.A_MONGODB_PASSWORD)
+console.log("a full host : ", process.env.A_FULL_HOST);
